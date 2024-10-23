45b6676c59d7886dd25fc665f8ec1721574c2bb8
Md Syful Islam <syful.islam3412.com>
2024-10-23 14:31:00 +0600

    Calculate the square of a number

---
+++ question_29.dart
@@ -0,0 +1,10 @@
+// Function to calculate the square of a number
+int square(int num) {
+  return num * num;
+}
+
+void main() {
+  int number = 5;
+  int result = square(number);
+  print('The square of $number is $result');
+}
