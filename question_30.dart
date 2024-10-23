e9d20985fcda5696c62441592a1e68afae22429f
Md Syful Islam <syful.islam3412.com>
2024-10-23 14:34:03 +0600

    Takes two numbers & return their sum

---
+++ question_30.dart
@@ -0,0 +1,16 @@
+// Function to calculate the sum of two numbers
+int sum(int num1, int num2) {
+  return num1 + num2;
+}
+
+void main() {
+  // Calling the function with two numbers
+  int number1 = 8;
+  int number2 = 12;
+
+  // Storing the result of the sum
+  int result = sum(number1, number2);
+
+  // Printing the result
+  print('The sum of $number1 and $number2 is $result');
+}
